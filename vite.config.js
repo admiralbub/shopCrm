8dc59b533699e6623b872295b6a7332354a0d170
Artem <admiral.bub@gmail.com>
2024-10-06 13:48:47 +0000

    Add js file in vite conf

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     server: {
         host: '192.168.2.132',
+        port:'8080',
         hot: true
     },
     css: {
@@ -15,7 +16,12 @@ export default defineConfig({
       },
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss','resources/scss/admin/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/scss/admin/app.scss', 
+                'resources/js/admin/admin.js',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
