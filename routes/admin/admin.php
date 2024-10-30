520bebeff9f89ec25cfc949e5f9338d8b15cdd62
Artem <admiral.bub@gmail.com>
2024-10-30 10:18:51 +0000

    Add file download in Admin

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryOrchidController;
 Route::post('/writesort_product', [CategoryOrchidController::class, 'editSort'])->name('admin.editSort');
+Route::group(['prefix' => 'filemanager'],  function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
